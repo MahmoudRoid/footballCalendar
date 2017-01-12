fa98fd410bd50300ad20e9a71b03232bdc24db1e
Mahmoud <m.mahmoudreza1@yahoo.com>
2017-01-12 11:28:56 +0330

    version sdk + db_team

--- app/src/main/java/mahmoud/footballcalendar/Database/db_Team.java
+++ app/src/main/java/mahmoud/footballcalendar/Database/db_Team.java
@@ -2,35 +2,33 @@ package mahmoud.footballcalendar.Database;
 
 import com.orm.SugarRecord;
 
-/**
- * Created by Mahmoud on 0031 8/31/2016.
- */
+
 public class db_Team extends SugarRecord {
-    public String MyTeamName,TeamHomeName,TeamAwayname ,date ,time ,status;
-    public int MatchDay,goalsHomeTeam,goalsAwayTeam;
+    public String myteamname,teamhomename,teamawayname ,date ,time ,status;
+    public int matchday,goalshometeam,goalsawayteam;
 
     public db_Team(){}
 
-    public db_Team(String MyTeamName,String TeamHomeName,String TeamAwayname, String date, String time,
-                String status,int MatchDay,int goalsHomeTeam,int goalsAwayTeam){
-        this.MyTeamName=MyTeamName;
-        this.TeamHomeName=TeamHomeName;
-        this.TeamAwayname=TeamAwayname;
+    public db_Team(String myteamname,String teamhomename,String teamawayname, String date, String time,
+                String status,int matchday,int goalshometeam,int goalsawayteam){
+        this.myteamname=myteamname;
+        this.teamhomename=teamhomename;
+        this.teamawayname=teamawayname;
         this.date=date;
         this.time=time;
         this.status=status;
-        this.MatchDay=MatchDay;
-        this.goalsHomeTeam=goalsHomeTeam;
-        this.goalsAwayTeam=goalsAwayTeam;
+        this.matchday=matchday;
+        this.goalshometeam=goalshometeam;
+        this.goalsawayteam=goalsawayteam;
     }
 
-    public String getMyTeamName(){return MyTeamName;}
-    public String getTeamHomeName(){return TeamHomeName;}
-    public String getTeamAwayname(){return TeamAwayname;}
+    public String getMyTeamName(){return myteamname;}
+    public String getTeamHomeName(){return teamhomename;}
+    public String getTeamAwayname(){return teamawayname;}
     public String getDate(){return date;}
     public String getTime(){return time;}
     public String getStatus(){return this.status;}
-    public int getMatchDay(){return MatchDay;}
-    public int getGoalsHomeTeam(){return goalsHomeTeam;}
-    public int getGoalsAwayTeam(){return goalsAwayTeam;}
+    public int getMatchDay(){return matchday;}
+    public int getGoalsHomeTeam(){return goalshometeam;}
+    public int getGoalsAwayTeam(){return goalsawayteam;}
 }
